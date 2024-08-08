3ddcad8555b4706f7c666a6afdbae5637dbc6ef0
Adam Patridge <patridge@gmail.com>
2024-08-08 15:04:43 -0600

    Update another round of build errors as best I can

--- DefConBadge2024/Pages/EnvironmentPage.cs
+++ DefConBadge2024/Pages/EnvironmentPage.cs
@@ -1,12 +1,17 @@
-﻿using Meadow.Devices;
+﻿using Meadow;
+using Meadow.Devices;
 using Meadow.Foundation;
 using Meadow.Foundation.Graphics;
-using Meadow.Foundation.Sensors.Accelerometers;
-using Meadow.Foundation.Sensors.Atmospheric;
-using Meadow.Foundation.Sensors.Light;
+// using Meadow.Foundation.Sensors.Motion;
+// using Meadow.Foundation.Sensors.Atmospheric;
+// using Meadow.Foundation.Sensors.Light;
+using Meadow.Peripherals.Sensors.Atmospheric;
+
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Meadow.Peripherals.Sensors;
+using Meadow.Peripherals.Sensors.Light;
 
 namespace DefConBadge2024
 {
@@ -15,9 +20,13 @@ namespace DefConBadge2024
         IProjectLabHardware config;
 
         MicroGraphics graphics;
-        Bme688 EnvironmentSensor { get; private set; }
-        Bh1750 LightSensor { get; private set; }
-        Bmi270 MotionSensor { get; private set; }
+        // Bme688 EnvironmentSensor { get; set; }
+        // Bh1750 LightSensor { get; set; }
+        // Bmi270 MotionSensor { get; set; }
+        ITemperatureSensor TemperatureSensor { get; set; }
+        IBarometricPressureSensor BarometricPressureSensor { get; set; }
+        IHumiditySensor HumiditySensor { get; set; }
+        ILightSensor LightSensor { get; set; }
 
         public bool IsUpdating = false;
 
@@ -28,9 +37,13 @@ namespace DefConBadge2024
 
             IsUpdating = true;
 
-            EnvironmentSensor.StartUpdating();
+            // EnvironmentSensor.StartUpdating();
+            // LightSensor.StartUpdating();
+            // MotionSensor.StartUpdating();
+            TemperatureSensor.StartUpdating();
+            BarometricPressureSensor.StartUpdating();
+            HumiditySensor.StartUpdating();
             LightSensor.StartUpdating();
-            MotionSensor.StartUpdating();
 
             Task.Run(() =>
             {
@@ -44,18 +57,26 @@ namespace DefConBadge2024
 
         public void StopUpdating()
         {
-            EnvironmentSensor.StartUpdating();
-            LightSensor.StartUpdating();
-            MotionSensor.StartUpdating();
+            // EnvironmentSensor.StartUpdating();
+            // LightSensor.StartUpdating();
+            // MotionSensor.StartUpdating();
+            TemperatureSensor.StopUpdating();
+            BarometricPressureSensor.StopUpdating();
+            HumiditySensor.StopUpdating();
+            LightSensor.StopUpdating();
 
             IsUpdating = false;
         }
 
         public void Init(IProjectLabHardware hardware)
         {
-            EnvironmentSensor = hardware.EnvironmentalSensor;
+            // EnvironmentSensor = hardware.EnvironmentalSensor;
+            // LightSensor = hardware.LightSensor;
+            // MotionSensor = hardware.MotionSensor;
+            TemperatureSensor = hardware.TemperatureSensor;
+            BarometricPressureSensor = hardware.BarometricPressureSensor;
+            HumiditySensor = hardware.HumiditySensor;
             LightSensor = hardware.LightSensor;
-            MotionSensor = hardware.MotionSensor;
         }
 
         public void Reset()
@@ -92,17 +113,17 @@ namespace DefConBadge2024
         {
             graphics.Clear();
           
-            if (config.EnvironmentalSensor.Temperature is { } temp)
+            if (config.TemperatureSensor.Temperature is { } temp)
             {
                 DrawStatus("Temperature:", $"{temp.Celsius:N1}C", WildernessLabsColors.GalleryWhite, 0);
             }
 
-            if (config.EnvironmentalSensor.Pressure is { } pressure)
+            if (config.BarometricPressureSensor.Pressure is { } pressure)
             {
                 DrawStatus("Pressure:", $"{pressure.StandardAtmosphere:N2}atm", WildernessLabsColors.GalleryWhite, 60);
             }
 
-            if (config.EnvironmentalSensor.Humidity is { } humidity)
+            if (config.HumiditySensor.Humidity is { } humidity)
             {
                 DrawStatus("Humidity:", $"{humidity.Percent:N1}%", WildernessLabsColors.GalleryWhite, 120);
             }
